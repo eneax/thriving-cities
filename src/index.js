c73a07a0762b48100ba69c4b8ef0ee2a8071016e
eneax <19818937+eneax@users.noreply.github.com>
2018-05-22 21:56:11 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
