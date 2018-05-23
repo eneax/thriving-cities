071349df2fa4839f90ef49f5ec9b2c85491272a4
eneax <19818937+eneax@users.noreply.github.com>
2018-05-23 14:48:45 +0200

    Create Contact Component

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import Header from './Header';
 import About from './About';
 import Events from './Events';
 import Cities from './Cities';
+import Contact from './Contact';
 
 (function($) {
   // Smooth scrolling with jQuery easing
@@ -99,6 +100,7 @@ class App extends Component {
         <About />
         <Events />
         <Cities />
+        <Contact />
       </div>
     );
   }
---
+++ src/components/Contact.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 mx-auto text-center">
+            <h2 className="section-heading">Let's Get In Touch!</h2>
+            <hr className="my-4" />
+            <p className="mb-5">Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-4 mx-auto text-center">
+            <i className="fas fa-envelope fa-3x mb-3 sr-contact"></i>
+            <p>
+              <a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a>
+            </p>
+          </div>
+          <div className="col-lg-4 mx-auto text-center">
+            <i className="fab fa-facebook-f fa-3x mb-3 sr-contact"></i>
+            <p>Facebook</p>
+          </div>
+          <div className="col-lg-4 mx-auto text-center">
+            <i className="fab fa-twitter fa-3x mb-3 sr-contact"></i>
+            <p>Twitter</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact;
