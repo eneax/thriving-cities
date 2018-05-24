6f2c7b27a3790ca6f8ebeabcc0a4f1b2c28da793
eneax <19818937+eneax@users.noreply.github.com>
2018-05-24 22:38:33 +0200

    Style Contact Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,29 +2,30 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <section id="contact" className="bg-white">
+    <section id="contact" className="bg-light">
       <div className="container">
         <div className="row">
           <div className="col-lg-8 mx-auto text-center">
-            <h2 className="section-heading">Let's Get In Touch!</h2>
+            <h2 className="section-heading text-uppercase">Contact Us</h2>
             <hr className="my-4" />
-            <p className="mb-5">Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+            <p className="mb-5">Thriving Cities is for all of the stakeholders working on and for the common good of their cities. For more information or to discuss this further, please do not hesitate to contact us.</p>
           </div>
         </div>
         <div className="row">
-          <div className="col-lg-4 mx-auto text-center">
-            <i className="fas fa-envelope fa-3x mb-3 sr-contact"></i>
-            <p>
-              <a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a>
-            </p>
+          <div className="col-4 mx-auto text-center">
+            <a href="mailto:communications@thrivingcitiesgroup.com" target="_blank" rel="noopener noreferrer">
+              <i className="fas fa-envelope fa-3x mb-3"></i>
+            </a>
           </div>
-          <div className="col-lg-4 mx-auto text-center">
-            <i className="fab fa-facebook-f fa-3x mb-3 sr-contact"></i>
-            <p>Facebook</p>
+          <div className="col-4 mx-auto text-center">
+            <a href="https://www.facebook.com/thrivingcities" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook-f fa-3x mb-3"></i>
+            </a>
           </div>
-          <div className="col-lg-4 mx-auto text-center">
-            <i className="fab fa-twitter fa-3x mb-3 sr-contact"></i>
-            <p>Twitter</p>
+          <div className="col-4 mx-auto text-center">
+            <a href="https://twitter.com/thriving_cities" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-twitter fa-3x mb-3"></i>
+            </a>
           </div>
         </div>
       </div>
