69fcb23eee108dbcd8c8d76be9db8459bf6721a4
eneax <19818937+eneax@users.noreply.github.com>
2018-05-24 22:48:04 +0200

    Style Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-white">
+    <footer className="bg-light">
       <div className="container">
         <div className="row">
           <div className="col-12 text-center">
