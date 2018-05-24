cd132d7356f4213483331f8d4d8c6205ec167b0d
eneax <19818937+eneax@users.noreply.github.com>
2018-05-24 22:02:35 +0200

    Style Cities Component

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import orlando from '../images/orlando.jpg';
+import portland from '../images/portland.jpg';
+import richmond from '../images/richmond.jpg';
+
 const Cities = () => {
   return (
     <section id="cities" className="bg-white">
@@ -9,71 +13,57 @@ const Cities = () => {
             <h2 className="section-heading text-uppercase">Cities</h2>
             <hr className="my-4"/>
           </div>
+          <div className="col-lg-8 mx-auto text-center">
+            <p className="mb-4">Cities are never blank slates. They have unique histories, identities, and cultures that have combined to form them into their current state and that continue to influence how they will develop in the future. To this end, in 2015 Thriving Cities commissioned field research in three pilot cities. Explore below.</p>
+          </div>
         </div>
-        <div className="row">
+        <div className="row mt-4">
           <div className="col-lg-12">
             <ul className="timeline">
               <li>
                 <div className="timeline-image">
-                  <img className="rounded-circle img-fluid" src="img/about/1.jpg" alt="" />
+                  <img className="rounded-circle img-fluid cities" src={portland} alt="Portland" />
                 </div>
                 <div className="timeline-panel">
                   <div className="timeline-heading">
-                    <h4>2009-2011</h4>
-                    <h4 className="subheading">Our Humble Beginnings</h4>
+                    <h4 className="subheading">Portland, Oregon</h4>
                   </div>
                   <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                    <p className="text-muted">Portland, to use a boxing cliché, punches above its weight. Or so it might seem in view of the way this isolated city of modest size and economic clout appears again and again in “best of” rankings of a seemingly endless variety.</p>
                   </div>
                 </div>
               </li>
               <li className="timeline-inverted">
                 <div className="timeline-image">
-                  <img className="rounded-circle img-fluid" src="img/about/2.jpg" alt="" />
+                  <img className="rounded-circle img-fluid cities" src={richmond} alt="Richmond" />
                 </div>
                 <div className="timeline-panel">
                   <div className="timeline-heading">
-                    <h4>March 2011</h4>
-                    <h4 className="subheading">An Agency is Born</h4>
+                    <h4 className="subheading">Richmond, Virginia</h4>
                   </div>
                   <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                    <p className="text-muted">Located in the heart of Old Dominion, Virginia’s capital city has been central to American, the Commonwealth of Virginia’s, and the South’s heritage and identity since the eighteenth century.</p>
                   </div>
                 </div>
               </li>
               <li>
                 <div className="timeline-image">
-                  <img className="rounded-circle img-fluid" src="img/about/3.jpg" alt="" />
+                  <img className="rounded-circle img-fluid cities" src={orlando} alt="Orlando" />
                 </div>
                 <div className="timeline-panel">
                   <div className="timeline-heading">
-                    <h4>December 2012</h4>
-                    <h4 className="subheading">Transition to Full Service</h4>
+                    <h4 className="subheading">Orlando, Florida</h4>
                   </div>
                   <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                    <p className="text-muted">Orlando is an adolescent living in the shadow of The Mouse. Growing fast, simultaneously unsure of herself and cocky, The City Beautiful struggles to embrace a cohesive identity apart from international tourism.</p>
                   </div>
                 </div>
               </li>
               <li className="timeline-inverted">
                 <div className="timeline-image">
-                  <img className="rounded-circle img-fluid" src="img/about/4.jpg" alt="" />
-                </div>
-                <div className="timeline-panel">
-                  <div className="timeline-heading">
-                    <h4>July 2014</h4>
-                    <h4 className="subheading">Phase Two Expansion</h4>
-                  </div>
-                  <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                  </div>
-                </div>
-              </li>
-              <li className="timeline-inverted">
-                <div className="timeline-image">
-                  <h4>Be Part
-                    <br/>Of Our
-                    <br/>Story!
+                  <h4 className="font-weight-bold">Time
+                    <br/>To
+                    <br/>Thrive!
                   </h4>
                 </div>
               </li>
