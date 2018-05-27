2601471097b22a64f423a7058172ca515e5db372
eneax <19818937+eneax@users.noreply.github.com>
2018-05-27 16:36:06 +0200

    Add Mailchimp to Modals

--- src/components/Events/Modals.js
+++ src/components/Events/Modals.js
@@ -24,9 +24,19 @@ const Modals = ({ id, full, heading, date, location, address, details }) => {
                   <p>
                     <strong>Details: </strong> {details}
                   </p>
-                  <button className="btn btn-signUp" type="button">
-                    Sign Up
-                  </button>
+                  <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css" />
+                  <div id="mc_embed_signup">
+                    <form action="https://coolwebsite.us12.list-manage.com/subscribe/post?u=d85f808a9af64d58bbb1fa212&amp;id=344d2f93f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" rel="noopener noreferrer" noValidate>
+                      <div id="mc_embed_signup_scroll">
+                        <div style={{ position: "absolute", left: "-5000px", ariaHidden: "true" }}>
+                          <input type="text" name="b_d85f808a9af64d58bbb1fa212_344d2f93f1" tabIndex="-1" value="" />
+                        </div>
+                        <div className="clear">
+                          <input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" className="btn btn-signUp" />
+                        </div>
+                      </div>
+                    </form>
+                  </div> 
                 </div>
               </div>
             </div>
--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -18,41 +18,9 @@ const NewsLetter = () => {
             </div>
           </form>
         </div> 
-
       </div>
     </section>
   )
 }
 
 export default NewsLetter;
-
-/*
-
-<section className="bg-white">
-  <div className="container text-center">
-    <h2 className="section-heading text-uppercase">Join Our Newsletter</h2>
-    <a className="btn btn-yellow btn-xl text-uppercase" href="http://eneaxharja.com">Subscribe</a>
-  </div>
-</section>
-
-*/ 
-
-
-
-/*
-
-<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css" />
-<div id="mc_embed_signup">
-  <form action="https://coolwebsite.us12.list-manage.com/subscribe/post?u=d85f808a9af64d58bbb1fa212&amp;id=344d2f93f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" rel="noopener noreferrer" noValidate>
-    <div id="mc_embed_signup_scroll">
-      <div style={{ position: "absolute", left: "-5000px", ariaHidden: "true" }}>
-        <input type="text" name="b_d85f808a9af64d58bbb1fa212_344d2f93f1" tabIndex="-1" value="" />
-      </div>
-      <div className="clear">
-        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="btn btn-yellow btn-xl text-uppercase" />
-      </div>
-    </div>
-  </form>
-</div> 
-
-*/ 
