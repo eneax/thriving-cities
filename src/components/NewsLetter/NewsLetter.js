c7e80c29a1aad0d6d1f5a05bbcdf801f27051d9e
eneax <19818937+eneax@users.noreply.github.com>
2018-05-28 11:43:58 +0200

    Style and comment NewsLetter

--- src/components/NewsLetter/NewsLetter.js
+++ src/components/NewsLetter/NewsLetter.js
@@ -6,10 +6,12 @@ const NewsLetter = () => {
     <section className="bg-white">
       <div className="container text-center">
         <h2 className="section-heading text-uppercase">Join Our Newsletter</h2>
+        {/* Begin MailChimp Signup Form */}
         <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css" />
         <div id="mc_embed_signup">
           <form action="https://coolwebsite.us12.list-manage.com/subscribe/post?u=d85f808a9af64d58bbb1fa212&amp;id=344d2f93f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" rel="noopener noreferrer" noValidate>
             <div id="mc_embed_signup_scroll">
+              {/* Do not remove this or risk form bot signups */}
               <div style={{ position: "absolute", left: "-5000px", ariaHidden: "true" }}>
                 <input type="text" name="b_d85f808a9af64d58bbb1fa212_344d2f93f1" tabIndex="-1" value="" />
               </div>
@@ -18,7 +20,8 @@ const NewsLetter = () => {
               </div>
             </div>
           </form>
-        </div> 
+        </div>
+        {/* End MailChimp Signup Form */}
       </div>
     </section>
   )
