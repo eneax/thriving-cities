fce832c222d835d99f80916fa4d74531256d75ec
eneax <19818937+eneax@users.noreply.github.com>
2018-05-29 08:35:43 +0200

    Update MailChimp Signup Form for NewsLetter

--- src/components/NewsLetter/NewsLetter.js
+++ src/components/NewsLetter/NewsLetter.js
@@ -9,7 +9,7 @@ const NewsLetter = () => {
         {/* Begin MailChimp Signup Form */}
         <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css" />
         <div id="mc_embed_signup">
-          <form action="https://coolwebsite.us12.list-manage.com/subscribe/post?u=d85f808a9af64d58bbb1fa212&amp;id=344d2f93f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" rel="noopener noreferrer" noValidate>
+          <form action="https://herokuapp.us12.list-manage.com/subscribe/post?u=d85f808a9af64d58bbb1fa212&amp;id=344d2f93f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" rel="noopener noreferrer" noValidate>
             <div id="mc_embed_signup_scroll">
               {/* Do not remove this or risk form bot signups */}
               <div style={{ position: "absolute", left: "-5000px", ariaHidden: "true" }}>
