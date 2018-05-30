cd39321b544f0787567004225ab7ef3a75c91716
eneax <19818937+eneax@users.noreply.github.com>
2018-05-30 13:33:36 +0200

    Add Material Design to Vimeo Video

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,15 +13,17 @@ const About = () => {
             <p className="mb-4">Thriving Cities is a group of unconventional urbanists with a unique understanding of, and approach to, cities.
             The group is committed to helping scholars, foundations, city officials, religious leaders, politicians, business people, nonprofits, and residents alike answer the question, what does it mean and take to thrive in my city and how can I contribute?</p>
             {/* Vimeo Video */}
-            <iframe 
-              className="col-12 my-3"
-              title="Thriving Cities"
-              src="https://player.vimeo.com/video/135908974"
-              width="640" height="360" frameBorder="0"
-              allowFullScreen="true"
-              webkitallowfullscreen="true"
-              mozallowfullscreen="true"
-            />       
+            <div className="embed-responsive embed-responsive-16by9 mb-5 material rounded border border-top-0 border-bottom-0 border-warning">
+              <iframe 
+                className="embed-responsive-item"
+                title="Thriving Cities"
+                src="https://player.vimeo.com/video/135908974"
+                width="640" height="360" frameBorder="0"
+                allowFullScreen="true"
+                webkitallowfullscreen="true"
+                mozallowfullscreen="true"
+              />
+            </div>     
             <a className="btn btn-yellow btn-xl text-uppercase js-scroll-trigger material" href="#contact">Get Started</a>
           </div>
         </div>
